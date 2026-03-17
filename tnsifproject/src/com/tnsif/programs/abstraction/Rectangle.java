9c03f99a4dc47e1a2b9f09ff92a517ae46d3b8a4
AnushaReddy121 <anushamulagundla6@gmail.com>
2026-03-17 22:41:20 +0530

    commit

---
+++ tnsifproject/src/com/tnsif/programs/abstraction/Rectangle.java
@@ -0,0 +1,21 @@
+package com.tnsif.programs.abstraction;
+
+public class Rectangle extends Shape{
+	int height;
+	int width;
+	
+	
+	public Rectangle(int height, int width) {
+		super();
+		this.height = height;
+		this.width = width;
+	}
+
+
+	@Override
+	int calArea() {
+		return area = height*width;	
+
+}
+}
+
